c19e948d25bc5d8c795712f0247be05776f791c5
kaffeInRacer <satriaaryaxi@gmail.com>
2023-06-21 13:30:32 +0700

    Cors test

--- routes/route.go
+++ routes/route.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	middleware2 "github.com/labstack/echo/v4/middleware"
-	"net/http"
 	"point-of-sale/app/controller"
 	"point-of-sale/app/controller/admin"
 	"point-of-sale/app/middleware"
@@ -11,12 +9,12 @@ import (
 )
 
 func Route(e *echo.Echo) {
-	config := middleware2.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
-		//AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "Image-Type", echo.HeaderAuthorization},
-	}
-	e.Use(middleware2.CORSWithConfig(config))
+	//config := middleware2.CORSConfig{
+	//	AllowOrigins: []string{"*"},
+	//	AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	//	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "Image-Type", echo.HeaderAuthorization},
+	//}
+	//e.Use(middleware2.CORSWithConfig(config))
 
 	api := e.Group("api/v1")
 	api.Static("/images", "./images")
